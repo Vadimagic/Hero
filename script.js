60f5db4202449014cb7cfc72aaaa453f5b0b5cd7
Vadim <vadim.prog.2000@gmail.com>
2021-04-07 02:31:40 +0300

    add stop when hover img

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let moveTop2 = false;
 
   let speedMove1 = 5;
-  let speedMove2 = 10;
+  let speedMove2 = 7;
+
+  let hoverPlay1 = false;
+  let hoverPlay2 = false;
 
   const togglePlay = (audioElement) => {
     audioElement.paused ? audioElement.play() : audioElement.pause();
@@ -26,32 +29,52 @@ document.addEventListener('DOMContentLoaded', () => {
     togglePlay(audioElement1);
   })
 
+  startPlay1.addEventListener('mouseover', () => {
+    hoverPlay1 = true;
+  })
+
+  startPlay1.addEventListener('mouseout', () => {
+    hoverPlay1 = false;
+  })
+
   startPlay2.addEventListener('click', () => {
     togglePlay(audioElement2);
   })
 
+  startPlay2.addEventListener('mouseover', () => {
+    hoverPlay2 = true;
+  })
+
+  startPlay2.addEventListener('mouseout', () => {
+    hoverPlay2 = false;
+  })
+
   function animate() {
-    startPlay1.style.left = leftPlay1 + 'px';
-    startPlay1.style.top = topPlay1 + 'px';
+    if (!hoverPlay1) {
+      startPlay1.style.left = leftPlay1 + 'px';
+      startPlay1.style.top = topPlay1 + 'px';
+  
+      if (leftPlay1 <= 0) moveLeft1 = false;
+      else if (leftPlay1 + startPlay1.offsetWidth >= windowWidth - speedMove1) moveLeft1 = true;
+      if (topPlay1 <= 0) moveTop1 = false;
+      else if (topPlay1 + startPlay1.offsetHeight >= windowHeight - speedMove1) moveTop1 = true;
+      
+      moveLeft1 ? leftPlay1-=speedMove1 : leftPlay1+=speedMove1;
+      moveTop1 ? topPlay1-=speedMove1 : topPlay1+=speedMove1;
+    }
 
-    startPlay2.style.left = leftPlay2 + 'px';
-    startPlay2.style.top = topPlay2 + 'px';
-
-    if (leftPlay1 <= 0) moveLeft1 = false;
-    else if (leftPlay1 + startPlay1.offsetWidth >= windowWidth - speedMove1) moveLeft1 = true;
-    if (topPlay1 <= 0) moveTop1 = false;
-    else if (topPlay1 + startPlay1.offsetHeight >= windowHeight - speedMove1) moveTop1 = true;
-
-    if (leftPlay2 <= 0) moveLeft2 = false;
-    else if (leftPlay2 + startPlay2.offsetWidth >= windowWidth - speedMove2) moveLeft2 = true;
-    if (topPlay2 <= 0) moveTop2 = false;
-    else if (topPlay2 + startPlay2.offsetHeight >= windowHeight - speedMove2) moveTop2 = true;
-
-    moveLeft1 ? leftPlay1-=speedMove1 : leftPlay1+=speedMove1;
-    moveTop1 ? topPlay1-=speedMove1 : topPlay1+=speedMove1;
-
-    moveLeft2 ? leftPlay2-=speedMove2 : leftPlay2+=speedMove2;
-    moveTop2 ? topPlay2-=speedMove2 : topPlay2+=speedMove2;
+    if (!hoverPlay2) {
+      startPlay2.style.left = leftPlay2 + 'px';
+      startPlay2.style.top = topPlay2 + 'px';
+      
+      if (leftPlay2 <= 0) moveLeft2 = false;
+      else if (leftPlay2 + startPlay2.offsetWidth >= windowWidth - speedMove2) moveLeft2 = true;
+      if (topPlay2 <= 0) moveTop2 = false;
+      else if (topPlay2 + startPlay2.offsetHeight >= windowHeight - speedMove2) moveTop2 = true;
+      
+      moveLeft2 ? leftPlay2-=speedMove2 : leftPlay2+=speedMove2;
+      moveTop2 ? topPlay2-=speedMove2 : topPlay2+=speedMove2;
+    }
     requestAnimationFrame(animate);
   }
 
